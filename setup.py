8533236f4e782138c3da9f919ab3175c0d95eb5b
Nick <opencalcorg@gmail.com>
2019-10-13 15:49:03 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OpenCalc',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Nick',
+    author_email='opencalcorg@gmail.com',
+    description=''
+)
