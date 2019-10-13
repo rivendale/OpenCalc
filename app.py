434d18746ce183505e3d7096407e49882851a019
Nick <opencalcorg@gmail.com>
2019-10-13 21:34:06 +0000

    ssh setups

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There! Testing App</h1>"
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
-
-if __name__ == '__main__':
-    app.run()
