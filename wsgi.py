4bb24305728d6c67b6759cdd11b37cc55a1e9606
Nick <opencalcorg@gmail.com>
2020-01-04 17:48:36 -0500

    open as application

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 #from opencalc import app as application
-from opencalc import app
+from opencalc import app as application
 
 if __name__ == "__main__":
     app.run()
 else:
     application = app
-
-    
