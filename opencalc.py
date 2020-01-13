13aa37057d442da5d4e4f2ded32be0e77af4c868
Nick <opencalcorg@gmail.com>
2020-01-12 21:35:24 -0500

    working on csv

--- opencalc.py
+++ opencalc.py
@@ -846,10 +846,14 @@ def downloadsymbols():
         proxy = io.StringIO()
         writer = csv.writer(proxy, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL )
 
-        writer.writerow(['Symbol','Next Earnings', 'Price Objective','Current Price'])
+        writeline = ['Symbol','Next Earnings', 'Price Objective','Current Price']
+        writeline = format(writeline)
+        writer.writerow(writeline)
 
         for record in records:
-            writer.writerow(format(record.symbol),format(record.nextearnings),format(record.priceobj),format(record.tprice))
+            writeline = [format(record.symbol),format(record.nextearnings),format(record.priceobj),format(record.tprice)]
+            writeline = format(writeline)
+            writer.writerow(writeline)
 
         # Creating the byteIO object from the StringIO Object
         b = bytes(proxy.getvalue(), 'utf-8')
