35899b296d46c3a370cc7a56a9d995c8e0f54423
Nick <opencalcorg@gmail.com>
2020-04-21 21:04:05 -0400

    updated requirements.txt

--- opencalcenv/bin/activate.fish
+++ opencalcenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nick/OpenCalc/opencalcenv"
+set -gx VIRTUAL_ENV "/home/nick/Desktop/OpenCalc/opencalcenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
